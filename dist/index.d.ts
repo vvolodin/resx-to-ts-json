38dc533beb8a0400366fe2178d608e8a52faa09d
Hasan Afzal <haafzal@microsoft.com>
2016-09-20 11:24:49 -0700

    Add ability to specify file name language for json files

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
 export declare function executeResxToTs(typeScriptResourcesNamespace: string, virtualResxFolder: string, virtualTypeScriptFolder: string): void;
-export declare function executeResxToJson(virtualResxFolder: string, virtualJsonFolder: string): void;
+export declare function executeResxToJson(virtualResxFolder: string, virtualJsonFolder: string, fileNameLanguage?: string): void;
